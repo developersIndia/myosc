b9b794e95e8e7ebc25560bc45189407defb2b0fc
Bhupesh-V <varshneybhupesh@gmail.com>
2020-11-08 19:17:01 +0530

    header-footer feature

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <p align="center">
   <h1 align="center">moc</h1>
   <p align="center">
-  <a href=""><img src="https://raw.githubusercontent.com/Bhupesh-V/memer-action/master/assets/logo.png" alt="moc-ction-logo" height="160"></a>
+  <a href=""><img src="https://raw.githubusercontent.com/Bhupesh-V/moc/main/assets/logo.png" alt="moc-ction-logo" height="160"></a>
   </p>
 </p>
--- main.py
+++ main.py
@@ -1,27 +1,20 @@
-#!/usr/bin/env python3
-
-# Utility to list a github user's pull requests in a nicely readable markdown file with 0 dependencies.
-#
-# Usage: ./my-prs.py <username>
-# Note that you will need commonmarker to render this md file (or just paste it in github readme)
-#
-# TODO:
-# - Handle Pagination (Done)
-# - Concurrent requests
-
 import urllib.parse
 import urllib.request
 import json, sys, os
-from dateutil import parser
+import pathlib
+from datetime import datetime
+from collections.abc import Mapping
 
 HEADERS = {"User-Agent": "Mozilla/5.0"}
+DEFAULT_HEADER = """"""
+DEFAULT_FOOTER = """"""
 
 
 def parse_link_header(link_header):
     header_links = link_header.split(",")[0].split(";")
     if header_links[1] == ' rel="next"':
         next_page = header_links[0]
-        # yo why tf response headers are normal ?
+        # yo why tf response headers are not normal ?
         next_page = next_page[1 : len(next_page) - 1]
     else:
         next_page = None
@@ -45,7 +38,11 @@ def handle_pagination(url):
     res_list = []
     while True:
         res, next_url = request(url)
-        res_list += res
+        if isinstance(res, Mapping):
+            # check if response is dict like
+            res_list += res["items"]
+        else:
+            res_list += res
         if next_url is None:
             break
         url = next_url
@@ -54,49 +51,80 @@ def handle_pagination(url):
 
 
 def get_user_repos(username):
-    # Why ?, to eliminate contributions to self repos
-    repo_list = []
-    repos, paginate = handle_pagination(
+    # eliminate contributions to self repos
+    print("Fetching User Repositories ...")
+    repos = handle_pagination(
         f"https://api.github.com/users/{username}/repos?type=owner&per_page=100"
     )
-    for repo in repos:
-        if repo["fork"] is False:
-            repo_list.append(repo["url"])
+    # filter forked repos
+    repo_list = [repo["url"] for repo in repos if repo["fork"] is False]
 
     return repo_list
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Needs a github username as an argument")
-        exit()
-    else:
-        user = sys.argv[1]
-
-    user_repos = []
-    print("Fetching User Repositories ...")
-    user_repos = get_user_repos(user)
+def get_user_prs(username):
     # get all pull requests of a user
-    print("Fetching {0}'s Pull Requests ...".format(user))
-    pull_requests, paginate = handle_pagination(
-        f"https://api.github.com/search/issues?q=author%3A{user}+type%3Apr&per_page=100"
+    print(f"Fetching {username}'s Pull Requests ...")
+    pull_requests = handle_pagination(
+        f"https://api.github.com/search/issues?q=author%3A{username}+type%3Apr&per_page=100"
     )
+    return pull_requests
 
-    with open(f"{user}-pull-requests.md", "w") as file:
-        file.write(
-            f"## [{user}](https://github.com/{user})'s open source contributions 🌟\n\n"
-        )
-        file.write("<ol>")
-        for pr in pull_requests["items"]:
-            if pr["repository_url"] not in user_repos:
-                created_date = parser.parse(pr["created_at"]).strftime("%d %b, %Y")
+
+def generate_report(repos, prs):
+    header = pathlib.Path("header.md")
+    footer = pathlib.Path("footer.md")
+
+    if header.exists():
+        with open(f"{user}-pull-requests.md", "w") as file:
+            header_file = open("header.md", "r")
+            file.write(header_file.read())
+            header_file.close()
+    else:
+        with open(f"{user}-pull-requests.md", "w") as file:
+            file.write(DEFAULT_HEADER)
+
+
+    with open(f"{user}-pull-requests.md", "a+") as file:
+        file.write("\n<ol>")
+        for pr in prs:
+            if pr["repository_url"] not in repos:
+                created_date = datetime.strptime(
+                    pr["created_at"], "%Y-%m-%dT%H:%M:%SZ"
+                ).strftime("%d %b, %Y")
                 file.write("\n<li>")
                 file.write(
-                    f"""<a target="_blank" href="{pr['html_url']}">{pr['title']}</a> in <b>{os.path.basename(pr['repository_url'])}</b> on <i>{created_date}</i> \n\n"""
+                    f"""<a target="_blank" href="{pr['html_url']}">{pr['title']}</a> in <b><a href="{pr['html_url'][:pr['html_url'].rfind('/pull/')]}">{os.path.basename(pr['repository_url'])}</a></b> on <i>{created_date}</i> \n\n"""
                 )
                 file.write(f"<details><summary>Description</summary>\n\n")
                 file.write(f"{pr['body']}\n</details>\n\n")
                 file.write("</li>")
         file.write("</ol>\n\n")
 
+    if footer.exists():
+        with open(f"{user}-pull-requests.md", "a+") as file:
+            footer_file = open("footer.md", "r")
+            file.write(footer_file.read())
+            footer_file.close()
+    else:
+        with open(f"{user}-pull-requests.md", "a+") as file:
+            file.write(DEFAULT_FOOTER)
+
+
     print(f"Saving context in {user}-pull-requests.md ✅")
+
+
+if __name__ == "__main__":
+    try:
+        user = os.environ["INPUT_USERNAME"]
+    except KeyError as e:
+        print("moc needs a github username")
+        exit()
+    # if len(sys.argv) != 2:
+    #     print("Needs a github username as an argument")
+    #     exit()
+    # else:
+    #     user = sys.argv[1]
+    user_repos = get_user_repos(user)
+    user_prs = get_user_prs(user)
+    generate_report(user_repos, user_prs)
